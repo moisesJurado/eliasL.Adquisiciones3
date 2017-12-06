25dc95c0d57efaee4f5745619916b3898bf69aef
moisesJurado <moises003@gmail.com>
2017-12-06 12:46:05 -0500

    se modifica y formatea MySqlDetalleRequerimiento

--- LP1_Proyecto_Semana_11/src/controlador/MySqlDetalleRequerimiento.java
+++ LP1_Proyecto_Semana_11/src/controlador/MySqlDetalleRequerimiento.java
@@ -2,45 +2,36 @@ package controlador;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-
 import entidad.DetalleRequerimiento;
 import util.MySqlConectar;
 
-public class MySqlDetalleRequerimiento {
-public int addDetalleRequerimiento (DetalleRequerimiento detaReq){
-	int estado=-1;
-	Connection cn=null;
-	PreparedStatement pstm=null;
-	try {
-		cn= new MySqlConectar().getConectar();
-		 String sql="insert into detalle_requerimiento values(?,?,?,?,?)";
-		 
-		   pstm=cn.prepareStatement(sql);
-		   	pstm.setInt(1, emp.getCodigo());
-		 	pstm.setString(2, emp.getNombre());
-		 	pstm.setString(3, emp.getApelli());
-		 	pstm.setString(4, emp.getDireccion());
-		 	pstm.setInt(5, emp.getCodDistr());
-		 	pstm.setString(6, emp.getFecnac());
-		 	pstm.setString(7, emp.getFono());
-		 	pstm.setString(8, emp.getCorreo());
-			pstm.setString(9, emp.getEstado());
-			pstm.setString(10, emp.getCell());
-			pstm.setInt(11, emp.getCodUs());
-			pstm.setInt(12, emp.getCodcar());
-		estado=pstm.executeUpdate();
-		
-	} catch (Exception e) {
-		e.printStackTrace();
-	}finally {
+class MySqlDetalleRequerimiento {
+	public int addDetalleRequerimiento(DetalleRequerimiento detaReq) {
+		int estado = -1;
+		Connection cn = null;
+		PreparedStatement pstm = null;
 		try {
-			if(pstm!=null)pstm.close();
-			if(cn!=null)cn.close();
-		} catch (Exception e2) {
-			e2.printStackTrace();
+			cn = new MySqlConectar().getConectar();
+			String sql = "insert into detalle_requerimiento values(?,?,?,?,?)";
+			pstm = cn.prepareStatement(sql);
+			pstm.setInt(1, detaReq.getCodigoRequerimiento());
+			pstm.setInt(2, detaReq.getCodigoBienes());
+			pstm.setInt(3, detaReq.getCantidadBienes());
+			pstm.setDouble(4, detaReq.getPrecioBase());
+			pstm.setString(5, detaReq.getObsBienes());
+			estado = pstm.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstm != null)
+					pstm.close();
+				if (cn != null)
+					cn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
 		}
+		return estado;
 	}
-	
-	return estado;
-}
 }
