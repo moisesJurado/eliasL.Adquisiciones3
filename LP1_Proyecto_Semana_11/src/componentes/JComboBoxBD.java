227f1ee2d089d28398c2dd404af59977a4aa2222
moisesJurado <moises003@gmail.com>
2017-12-06 14:22:23 -0500

    se modifica jComboBoxBd y con esto cargamos los bienes de frmReq
    tb se hace format de las 2 clases estas

--- LP1_Proyecto_Semana_11/src/componentes/JComboBoxBD.java
+++ LP1_Proyecto_Semana_11/src/componentes/JComboBoxBD.java
@@ -3,45 +3,53 @@ package componentes;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+
 import javax.swing.JComboBox;
+
+import entidad.Bienes;
 import util.MySqlConectar;
 
 public class JComboBoxBD extends JComboBox {
-	
-	public JComboBoxBD (String sql){ //sql --> select cod_dis, nom_dis, from tb_distrito
-		
+
+	public JComboBoxBD(String sql) { // sql --> select cod_dis, nom_dis, from tb_distrito
+
 		Connection cn = null;
 		PreparedStatement pstm = null;
-		ResultSet rs = null; //guarda todo lo que se hace de sql y se devuelve al gestor
-		
+		ResultSet rs = null; // guarda todo lo que se hace de sql y se devuelve al gestor
+
 		try {
-			
+
 			cn = new MySqlConectar().getConectar();
 			pstm = cn.prepareStatement(sql);
 			rs = pstm.executeQuery();
-			
-			while (rs.next()){
-				addItem(rs.getString(2)); 
+
+			while (rs.next()) {
+				addItem(rs.getString(2));
 				// hace que se muestre la segunda columna de tu combito que es el parametro
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			try {
-				
-				if (rs != null) rs.close();
-				if (pstm != null) pstm.close();
-				if (cn != null) cn.close();
-				
+
+				if (rs != null)
+					rs.close();
+				if (pstm != null)
+					pstm.close();
+				if (cn != null)
+					cn.close();
+
 			} catch (Exception e2) {
 				e2.printStackTrace();
 			}
 		}
 	}
-	
-	
-	
 
+	public JComboBoxBD(ArrayList<Bienes> input) {
+		for (Bienes i : input) {
+			this.addItem(i.nombre);
+		}
+	}
 }
--- LP1_Proyecto_Semana_11/src/gui/frmRequerimiento.java
+++ LP1_Proyecto_Semana_11/src/gui/frmRequerimiento.java
@@ -8,6 +8,8 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
+import componentes.JComboBoxBD;
+import controlador.MySqlBienes;
 import controlador.MySqlDetalleRequerimiento;
 import controlador.MySqlRequerimiento;
 import entidad.DetalleRequerimiento;
@@ -65,90 +67,90 @@ public class frmRequerimiento extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblRequerimiento = new JLabel("EMITIR REQUERIMIENTO");
 		lblRequerimiento.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblRequerimiento.setBounds(237, 22, 216, 14);
 		contentPane.add(lblRequerimiento);
-		
+
 		JLabel lblCdigo = new JLabel("C\u00F3digo Requerimiento");
 		lblCdigo.setBounds(10, 60, 111, 14);
 		contentPane.add(lblCdigo);
-		
+
 		JLabel lblCdigoEmpleado = new JLabel("C\u00F3digo Empleado");
 		lblCdigoEmpleado.setBounds(264, 57, 92, 14);
 		contentPane.add(lblCdigoEmpleado);
-		
+
 		JLabel lblFecha = new JLabel("Fecha de Emisi\u00F3n");
 		lblFecha.setBounds(508, 57, 82, 14);
 		contentPane.add(lblFecha);
-		
+
 		JLabel lblFechaDeEntrega = new JLabel("Fecha de Entrega");
 		lblFechaDeEntrega.setBounds(10, 88, 111, 14);
 		contentPane.add(lblFechaDeEntrega);
-		
+
 		txtCodRequerimiento = new JTextField();
 		txtCodRequerimiento.setBounds(125, 57, 86, 20);
 		contentPane.add(txtCodRequerimiento);
 		txtCodRequerimiento.setColumns(10);
-		
+
 		txtCodEmpleado = new JTextField();
 		txtCodEmpleado.setColumns(10);
 		txtCodEmpleado.setBounds(367, 54, 86, 20);
 		contentPane.add(txtCodEmpleado);
-		
+
 		txtFechaEmision = new JTextField();
 		txtFechaEmision.setEditable(false);
 		txtFechaEmision.setColumns(10);
 		txtFechaEmision.setBounds(604, 54, 86, 20);
 		contentPane.add(txtFechaEmision);
-		
+
 		txtFechaEntrega = new JTextField();
 		txtFechaEntrega.setColumns(10);
 		txtFechaEntrega.setBounds(125, 85, 86, 20);
 		contentPane.add(txtFechaEntrega);
-		
+
 		JLabel lblElegirBienesDe = new JLabel("Elegir bien");
 		lblElegirBienesDe.setBounds(10, 113, 73, 14);
 		contentPane.add(lblElegirBienesDe);
-		
-		JComboBox cboBienes = new JComboBox();
+
+		JComboBox cboBienes = new JComboBoxBD(new MySqlBienes().listBienes());
 		cboBienes.setBounds(125, 113, 565, 17);
 		contentPane.add(cboBienes);
-		
+
 		JSpinner spCantidad = new JSpinner();
 		spCantidad.setBounds(125, 200, 86, 20);
 		contentPane.add(spCantidad);
-		
+
 		JLabel lblCantidad = new JLabel("Cantidad");
 		lblCantidad.setBounds(10, 202, 73, 14);
 		contentPane.add(lblCantidad);
-		
+
 		JLabel lblObservaciones = new JLabel("Observaciones");
 		lblObservaciones.setBounds(10, 138, 73, 14);
 		contentPane.add(lblObservaciones);
-		
+
 		txtObservaciones = new JTextField();
 		txtObservaciones.setBounds(125, 142, 565, 47);
 		contentPane.add(txtObservaciones);
 		txtObservaciones.setColumns(10);
-		
+
 		JButton btnPasar = new JButton("Pasar");
 		btnPasar.setBounds(321, 199, 89, 23);
 		contentPane.add(btnPasar);
-		
+
 		JButton btnEliminar = new JButton("Eliminar");
 		btnEliminar.setBounds(513, 199, 89, 23);
 		contentPane.add(btnEliminar);
-		
+
 		JButton btnGenerar = new JButton("Generar");
 		btnGenerar.setBounds(309, 387, 89, 23);
 		contentPane.add(btnGenerar);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 231, 680, 142);
 		contentPane.add(scrollPane);
-		
+
 		tblDetalleRequerimiento = new JTable();
 		scrollPane.setViewportView(tblDetalleRequerimiento);
 		modelo.addColumn("Código");
